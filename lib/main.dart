d69cb6e98eb57ad2515c242b75195a0101f2034c
LauraPulidoCBA <laurapulido.cba.sena@gmail.com>
2026-03-06 21:28:58 -0500

    Agregué scroll con SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(home: PokemonPage());
 }
 
 
-class PokemonPage extends StatefulWidget { @override _PokemonPageState createState() => _PokemonPageState(); }
+class PokemonPage extends StatefulWidget { 
+  @override 
+  _PokemonPageState createState() => _PokemonPageState(); 
+}
+
+
 class _PokemonPageState extends State<PokemonPage> {
   @override
-  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text("Lista de Pokemons")));
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text("Lista de Pokemons")),
+    body: SingleChildScrollView(
+      child: Column(
+        children: []
+      ),
+    ),
+  );
 }
