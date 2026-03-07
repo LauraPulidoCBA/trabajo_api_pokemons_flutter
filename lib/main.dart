f8a4121410144babedd900cfa35f938ff29f1bd4
LauraPulidoCBA <laurapulido.cba.sena@gmail.com>
2026-03-06 21:20:47 -0500

    Agregue pantalla principal con Scaffold y AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(home: PokemonPage());
 }
+
+
+class PokemonPage extends StatefulWidget { @override _PokemonPageState createState() => _PokemonPageState(); }
+class _PokemonPageState extends State<PokemonPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text("Lista de Pokemons")));
+}
