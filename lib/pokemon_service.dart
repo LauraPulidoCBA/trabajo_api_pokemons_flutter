27b90ad31fda4d505a3b949c42f7e276c7c07a43
natalia2208 <collazosk25@gmail.com>
2026-03-06 21:42:17 -0500

    Implementar peticion HTTP

--- lib/pokemon_service.dart
+++ lib/pokemon_service.dart
@@ -1,3 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-class PokemonService { }
+class PokemonService {
+
+  Future<List> getPokemons() async {
+
+  var url = Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=20");
+
+  var response = await http.get(url);
+  
+  return []; 
+}
+ }
