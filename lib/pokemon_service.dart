311b0eb5a0008210b1169917a8ddaa6a9cdc83b6
natalia2208 <collazosk25@gmail.com>
2026-03-06 21:54:23 -0500

    conectar el servicio con la pantalla principal

--- lib/pokemon_service.dart
+++ lib/pokemon_service.dart
@@ -10,8 +10,17 @@ class PokemonService {
   if(response.statusCode == 200){
   var data = json.decode(response.body);
   return data["results"];
-}
 
-  return []; 
+}else{
+
+   return []; 
 }
  }
+ Future<Map<String, dynamic>>
+  getPokemonDetail(int id) async {
+  var url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$id");
+  var response = await http.get(url);
+
+  
+  return json.decode(response.body);
+}
