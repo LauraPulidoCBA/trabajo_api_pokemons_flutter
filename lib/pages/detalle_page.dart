4aed47888b0dbe8895ed31c65594a000703fe708
herreracarrenoyeferso <herreracarreoyeferso@gmail.com>
2026-03-06 22:36:49 -0500

    Agregar imagen del pokemon

---
+++ lib/pages/detalle_page.dart
@@ -0,0 +1,17 @@
+Dart
+import 'package:flutter/material.dart';
+class DetallePage extends StatefulWidget {
+  final String nombre; final int id;
+  DetallePage({required this.nombre, required this.id});
+  @override _DetallePageState createState() => _DetallePageState();
+}
+
+Dart
+onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DetallePage(nombre: name, id: id)))
+
+
+Dart
+body: Column(children: [ Text(widget.nombre.toUpperCase()) ])
+
+Dart
+Image.network("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${widget.id}.png")
