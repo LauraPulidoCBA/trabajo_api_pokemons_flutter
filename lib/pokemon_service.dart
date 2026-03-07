7954b84aa7ed67226c15e817cafaa50af1740e54
natalia2208 <collazosk25@gmail.com>
2026-03-06 21:47:40 -0500

    convertir datos JSON de la API

--- lib/pokemon_service.dart
+++ lib/pokemon_service.dart
@@ -7,7 +7,11 @@ class PokemonService {
   var url = Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=20");
 
   var response = await http.get(url);
-  
+  if(response.statusCode == 200){
+  var data = json.decode(response.body);
+  return data["results"];
+}
+
   return []; 
 }
  }
