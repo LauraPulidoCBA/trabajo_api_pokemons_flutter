cd985d357c2012dda20810f4b5f0fd78d60214f5
natalia2208 <collazosk25@gmail.com>
2026-03-06 21:57:05 -0500

    Correxion de errores en carga de datos

--- lib/pokemon_service.dart
+++ lib/pokemon_service.dart
@@ -1,26 +1,27 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 class PokemonService {
-
   Future<List> getPokemons() async {
+    var url = Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=20");
 
-  var url = Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=20");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      return data["results"];
+    } else {
+      return [];
+    }
+  }
 
-  var response = await http.get(url);
-  if(response.statusCode == 200){
-  var data = json.decode(response.body);
-  return data["results"];
+  Future<Map<String, dynamic>> getPokemonDetail(int id) async {
+    var url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$id");
+    var response = await http.get(url);
 
-}else{
-
-   return []; 
-}
- }
- Future<Map<String, dynamic>>
-  getPokemonDetail(int id) async {
-  var url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$id");
-  var response = await http.get(url);
-
-  
-  return json.decode(response.body);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      return {};
+    }
+  }
 }
