d8fddf939cc6c3b69847c035e6b16e547dce053b
santiagobernal123 <santiagobarevalo05@gmail.com>
2026-03-09 13:41:25 -0500

    Se implementa Hero para animación de imagen de Pokemon

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(home: PokemonPage());
 }
 
-
-class PokemonPage extends StatefulWidget { 
-  @override 
-  _PokemonPageState createState() => _PokemonPageState(); 
+class PokemonPage extends StatefulWidget {
+  @override
+  _PokemonPageState createState() => _PokemonPageState();
 }
-
 
 class _PokemonPageState extends State<PokemonPage> {
   List pokemons = [
@@ -20,23 +18,49 @@ class _PokemonPageState extends State<PokemonPage> {
     {"name": "Charmander"},
     {"name": "Bulbasaur"},
   ];
-  
+
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: Text("Lista de Pokemons")),
-    body: SingleChildScrollView(
-      child: Column(
-        children: List.generate(
-          pokemons.length, 
-          (index) => Card(
-            margin: EdgeInsets.all(8),
-            child: Padding(
-              padding: EdgeInsets.all(12),
-              child: Text(pokemons[index]["name"]),
+        appBar: AppBar(title: Text("Lista de Pokemons")),
+        body: SingleChildScrollView(
+          child: Column(
+            children: List.generate(
+              pokemons.length,
+              (index) {
+                int id = index + 1; 
+
+                return Card(
+                  margin: EdgeInsets.all(8),
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        Hero(
+                          tag: "pokemon$id",
+                          child: Image.network(
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png",
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(
+                          child: Text(
+                            pokemons[index]["name"].toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ),
-      ),
-    ),
-  );
+      );
 }
