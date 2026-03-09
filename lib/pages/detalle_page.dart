24b6e4c4e4ec3ba2bf297ca676327e3cc0e7b48c
santiagobernal123 <santiagobarevalo05@gmail.com>
2026-03-09 14:24:43 -0500

    Pruebas finales de la aplicacion

--- lib/pages/detalle_page.dart
+++ lib/pages/detalle_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class DetallePage extends StatefulWidget {
   final String nombre;
   final int id;
@@ -21,32 +20,43 @@ class _DetallePageState extends State<DetallePage> {
       appBar: AppBar(
         title: Text(widget.nombre),
       ),
-      body: Column(
-        children: [
 
-          Hero(
-            tag: "pokemon${widget.id}",
-            child: Image.network(
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${widget.id}.png",
-              width:  250,
-              height: 250,
+      
+      body: detalle.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+
+                Hero(
+                  tag: "pokemon${widget.id}",
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${widget.id}.png",
+                    width: 250,
+                    height: 250,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  widget.nombre.toUpperCase(),
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  "Altura: ${detalle["height"]}",
+                  textAlign: TextAlign.center,
+                ),
+
+                Text(
+                  "Peso: ${detalle["weight"]}",
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-          ),
-
-          SizedBox(height: 20),
-
-          Text(
-            widget.nombre.toUpperCase(),
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-
-          SizedBox(height: 20),
-
-          Text("Altura: ${detalle["height"]}"),
-          Text("Peso: ${detalle["weight"]}"),
-
-        ],
-      ),
     );
   }
 }
